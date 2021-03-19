13e3cde5d69a62f04950bc15709a71fd605c2877
Leon Dawson-Couper <64979109+ItsLeon15@users.noreply.github.com>
2021-03-19 12:13:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ client.on('ready', () => { // When the bot turns on
         const statuses = [
             `on ${client.guilds.cache.size} servers`, // Enables the bot to show how many servers it's in, in the status
             `on ${client.channels.cache.size} channels`, // Enables the bot to show how many channels it's in, in the status
+            "to music...", // Enables the bot to send a message of your choice
         ]
 
-        const status = statuses[Math.floor(Math.random() * statuses.length)]
+        const status = statuses[Math.floor(Math.random() * statuses.length)] // Chooses a random list from statuses and puts it into a variable.
         client.user.setActivity(status, { type: "LISTENING" }) // Status changer - WATCHING / LISTENING / STREAMING / DND / ONLINE
 
     }, 
 
-            20000) // Time for status to change - Recommended  = 20,000 (20 Seconds) - API doesn't really allow less values but it will work
+            30000) // Time for status to change - Recommended  = 20,000 (20 Seconds) - API doesn't really allow less values but it will work
 
 });
